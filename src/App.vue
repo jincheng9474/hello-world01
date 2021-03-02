7ff0715a56e03ca716cb83724b81da579f46bcd4
zjc <1206904379@qq.com>
2021-03-02 20:35:34 +0800

    初次部署  图片

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <p>java0821 全体同学 貌美如花</p>
       <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
       <p>祝 java0821 6666666</p>
-   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+   <img src="E:\壁纸\98.png"/>
   </div>
 </template>
 
